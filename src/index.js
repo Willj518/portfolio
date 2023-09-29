aed3cdfef30b1e6f52872a37e9f261d77f497d53
William W <69099196+Willj518@users.noreply.github.com>
2023-09-29 11:59:26 -0700

    .
    ,

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import ThemeContextProvider from './contexts/theme-context';
-import './';
+import './index.css';
 // import reportWebVitals from './reportWebVitals';
 const theme = createTheme();
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './app';
+// import ThemeContextProvider from './contexts/theme-context';
+// import './';
+// import reportWebVitals from './reportWebVitals';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <ThemeContextProvider>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//   </ThemeContextProvider>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
